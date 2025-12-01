44a58b09df3af800d15d521432b86f55254b45e2
Aaron-Vuong <achvuong@gmail.com>
2025-11-30 23:54:54 -0800

    Adding Pinned Component to Quartz.

---
+++ quartz/components/Pinned.tsx
@@ -0,0 +1,54 @@
+import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
+import style from "./styles/pinned.scss"
+import { resolveRelative} from "../util/path"
+// import { i18n } from "../i18n"
+import { classNames } from "../util/lang"
+import OverflowListFactory from "./OverflowList"
+
+interface PinnedOptions {
+  hideWhenEmpty: boolean
+}
+
+const defaultOptions: PinnedOptions = {
+  hideWhenEmpty: true,
+}
+
+export default ((opts?: Partial<PinnedOptions>) => {
+  const options: PinnedOptions = { ...defaultOptions, ...opts }
+  const { OverflowList, overflowListAfterDOMLoaded } = OverflowListFactory()
+
+  const Pinned: QuartzComponent = ({
+    fileData,
+    allFiles,
+    displayClass,
+    cfg,
+  }: QuartzComponentProps) => {
+    const pinnedFiles = allFiles.filter((file) => file.frontmatter?.pinned)
+    if (options.hideWhenEmpty && pinnedFiles.length == 0) {
+      return null
+    }
+    // TODO: To make this language dependent, use the below instead.
+    // i18n(cfg.locale).components.pinned.title
+    return (
+      <div class={classNames(displayClass, "pinned")}>
+        <h3>Pinned</h3> 
+        <OverflowList>
+          {(
+            pinnedFiles.map((f) => (
+              <li>
+                <a href={resolveRelative(fileData.slug!, f.slug!)} class="internal">
+                  {f.frontmatter?.motd}
+                </a>
+              </li>
+            ))
+          )}
+        </OverflowList>
+      </div>
+    )
+  }
+
+  Pinned.css = style
+  Pinned.afterDOMLoaded = overflowListAfterDOMLoaded
+
+  return Pinned
+}) satisfies QuartzComponentConstructor
