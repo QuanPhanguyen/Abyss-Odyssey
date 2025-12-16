736aea554716eeeb9fd64bac560a2f3ce2d9cebf
Aaron-Vuong <achvuong@gmail.com>
2025-12-16 02:26:34 -0800

    Adding SHA versioning display

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -2,6 +2,8 @@ import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } fro
 import style from "./styles/footer.scss"
 import { version } from "../../package.json"
 import { i18n } from "../i18n"
+import { readFileSync } from "fs"
+
 
 interface Options {
   links: Record<string, string>
@@ -11,11 +13,18 @@ export default ((opts?: Options) => {
   const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
     const year = new Date().getFullYear()
     const links = opts?.links ?? []
+    let sha: string = "UNKNOWN"
+    try {
+      sha = readFileSync("git_sha", "utf-8")
+    } catch(error) {
+      console.error("Unable to read `git_sha` file to display version.")
+    }
     return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
           <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+          <p>Version: {sha} </p>
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
