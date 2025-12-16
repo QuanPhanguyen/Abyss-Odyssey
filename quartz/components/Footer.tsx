323ae60e951264fe4517ac66ff475e52455e80f1
Aaron-Vuong <achvuong@gmail.com>
2025-12-16 02:52:53 -0800

    Bake version.json and refer to it as an artifact.

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -1,8 +1,8 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import style from "./styles/footer.scss"
 import { version } from "../../package.json"
+import { git_sha } from "../../version.json"
 import { i18n } from "../i18n"
-import { readFileSync } from "fs"
 
 
 interface Options {
@@ -13,18 +13,12 @@ export default ((opts?: Options) => {
   const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
     const year = new Date().getFullYear()
     const links = opts?.links ?? []
-    let sha: string = "UNKNOWN"
-    try {
-      sha = readFileSync("git_sha", "utf-8")
-    } catch(error) {
-      console.error("Unable to read `git_sha` file to display version.")
-    }
     return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
           <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
-          <p>Version: {sha} </p>
+          <p>Version: {git_sha} </p>
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
