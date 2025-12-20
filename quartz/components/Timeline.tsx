e83361333f9412c4d4ef3da03d9c8fa96b661284
Aaron-Vuong <achvuong@gmail.com>
2025-12-19 23:44:26 -0800

    Make basic template for Timeline

--- quartz/components/Timeline.tsx
+++ quartz/components/Timeline.tsx
@@ -1,29 +1,27 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import style from "./styles/timeline.scss"
-import { resolveRelative} from "../util/path"
 // import { i18n } from "../i18n"
 import { classNames } from "../util/lang"
 import OverflowListFactory from "./OverflowList"
 
-interface PinnedOptions {
+interface TimelineOptions {
   hideWhenEmpty: boolean
 }
 
-const defaultOptions: PinnedOptions = {
+const defaultOptions: TimelineOptions = {
   hideWhenEmpty: true,
 }
 
-export default ((opts?: Partial<PinnedOptions>) => {
-  const options: PinnedOptions = { ...defaultOptions, ...opts }
+export default ((opts?: Partial<TimelineOptions>) => {
+  const options: TimelineOptions = { ...defaultOptions, ...opts }
   const { OverflowList, overflowListAfterDOMLoaded } = OverflowListFactory()
 
-  const Pinned: QuartzComponent = ({
+  const Timeline: QuartzComponent = ({
     fileData,
     allFiles,
     displayClass,
   }: QuartzComponentProps) => {
-    const pinnedFiles = allFiles.filter((file) => file.frontmatter?.pinned)
-    if (options.hideWhenEmpty && pinnedFiles.length == 0) {
+    if (options.hideWhenEmpty) {
       return null
     }
     // TODO: To make this language dependent, use the below instead.
@@ -32,22 +30,14 @@ export default ((opts?: Partial<PinnedOptions>) => {
       <div class={classNames(displayClass, "pinned")}>
         <h3>Pinned</h3> 
         <OverflowList>
-          {(
-            pinnedFiles.map((f) => (
-              <li>
-                <a href={resolveRelative(fileData.slug!, f.slug!)} class="internal">
-                  {f.frontmatter?.motd}
-                </a>
-              </li>
-            ))
-          )}
+          {}
         </OverflowList>
       </div>
     )
   }
 
-  Pinned.css = style
-  Pinned.afterDOMLoaded = overflowListAfterDOMLoaded
+  Timeline.css = style
+  Timeline.afterDOMLoaded = overflowListAfterDOMLoaded
 
-  return Pinned
+  return Timeline
 }) satisfies QuartzComponentConstructor
